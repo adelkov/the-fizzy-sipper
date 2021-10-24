72f7e4fcf51f47d7444f42b491377aa54013b963
Adel <kovacsadel12@gmail.com>
2021-10-24 10:37:42 +0200

    chore: update readme with test info, add default API URL to config, move dev deps to correct field in package.json

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: process.env.REACT_APP_BASE_URL ?? ''
+  baseURL: process.env.REACT_APP_BASE_URL ?? 'https://www.thecocktaildb.com/api/json/v1/1/'
 }
 
 export default config
